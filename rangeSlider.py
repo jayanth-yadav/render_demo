2cd5c4128a4867394bd11d7420a1879cfa146af2
jayanth-yadav <83997869+jayanth-yadav@users.noreply.github.com>
2023-03-02 09:49:15 +0530

    Add files via upload

---
+++ rangeSlider.py
@@ -0,0 +1,25 @@
+# Imports
+from dash import Dash, html, dcc, Input, Output
+
+# App
+app = Dash(__name__)
+# Layout
+app.layout = html.Div(children=[
+    html.Div(
+        dcc.RangeSlider(-5,6,1,id='slider',value=[1,2]),
+        style=dict(width='50%')
+    ),
+    html.H1(id='out')
+])
+
+# Callbacks
+@app.callback(
+        Output('out','children'),
+        Input('slider','value')
+)
+def callback(value):
+    return value[0]*value[1]
+
+# Server
+if __name__ == "__main__":
+    app.run_server(debug=True)
